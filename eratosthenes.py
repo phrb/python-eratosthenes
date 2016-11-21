985487aabcdb20a90f5249a4bb9dffe0ebf0ba5b
phrb <pedro.bruel@gmail.com>
2016-11-21 14:28:04 -0200

    Fix for python3

--- eratosthenes.py
+++ eratosthenes.py
@@ -1,11 +1,11 @@
 def sieve(n):
     not_prime = []
     prime = []
-    for i in xrange(2, n+1):
+    for i in range(2, n+1):
         if i not in not_prime:
             prime.append(i)
-            for j in xrange(i*i, n+1, i):
+            for j in range(i*i, n+1, i):
                 not_prime.append(j)
     return prime
 
-print sieve(50)
+print(sieve(50))
